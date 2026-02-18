b1f458ebe9bc1d0c92d1d7f593ff8d25613db26a
Rhonaiye Felix <rodivinex@gmail.com>
2026-02-18 15:23:59 +0100

    updated cors

--- src/helpers/cors.ts
+++ src/helpers/cors.ts
@@ -3,8 +3,10 @@ import cors from 'cors';
 const allowedOrigins = [
   'http://localhost:4000',
   'https://localhost:4000',
+  'https://www.ruutz.app',
 ];
 
 export const internalOptions: cors.CorsOptions = {
-  origin: '*',
+  origin: allowedOrigins,
+  credentials: true,
 };
